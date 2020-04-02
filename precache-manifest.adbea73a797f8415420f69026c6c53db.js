934f99cf47824e36ae9c3d2d54900c2c1eed757a
Alex Liu <sotework@outlook.com>
2020-04-02 17:57:36 +0800

    modify

---
+++ precache-manifest.adbea73a797f8415420f69026c6c53db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128acfdcbdebc8b86bbaf377d3d2c6de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f22e8bd72312a1c2ba5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b980dcd84747388dd52c",
+    "url": "/static/js/2.939cd06d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.939cd06d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f73cba5e8127c30ce33",
+    "url": "/static/js/3.1418513c.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/static/js/3.1418513c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e0ebe73a5d52215a5e",
+    "url": "/static/js/4.c692c425.chunk.js"
+  },
+  {
+    "revision": "4f22e8bd72312a1c2ba5",
+    "url": "/static/js/main.d3e0b88a.chunk.js"
+  },
+  {
+    "revision": "219febd3d12f22f1b9e8",
+    "url": "/static/js/runtime-main.cdf17244.js"
+  }
+]);
